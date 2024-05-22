51a6e81cf613d402339a17d419d37eaa0703b2a2
iamRazzakk <rakibt23p@gmail.com>
2024-05-22 01:18:26 -0400

    basic project done

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,13 @@
-import express, { Application, Request, Response } from "express"
-const app:Application = express()
-import cors from "cors"
-const port = 3000
+import express, { Application, Request, Response } from 'express';
+const app: Application = express();
+import cors from 'cors';
 // parser
-app.use(express.json())
-app.use(cors)
+app.use(express.json());
+app.use(cors);
 
+app.get('/', (req: Request, res: Response) => {
+  res.send('hello world');
+});
+console.log(process.cwd());
 
-
-app.get('/', (req:Request, res:Response) => {   
-  res.send('Hello World!')
-})
-console.log(process.cwd())
-
-export default app
+export default app;
