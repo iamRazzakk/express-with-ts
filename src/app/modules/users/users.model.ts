f349c72b0103905067a8c6e888aababe9e106226
iamRazzakk <rakibt23p@gmail.com>
2024-05-27 20:44:11 -0400

    create model

--- src/app/modules/users/users.model.ts
+++ src/app/modules/users/users.model.ts
@@ -1,5 +1,34 @@
-import { Schema } from "mongoose";
+import { Schema, model } from "mongoose";
+import { TUser } from "./users.interface";
 
-const userSchema = new Schema({
-
-})
+const userSchema = new Schema<TUser>({
+    id: {
+        type: String,
+        require: true
+    },
+    password: {
+        type: String,
+        require: true
+    },
+    needPasswordChange: {
+        type: Boolean,
+        default: true
+    },
+    role: {
+        type: String,
+        enum: ["admin", "student", "faculty"],
+        require: true
+    },
+    status: {
+        type: String,
+        enum: ["in-progress", "blocked"],
+        require: true
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    }
+}, {
+    timestamps: true
+},)
+export const User = model<TUser>("User", userSchema)
