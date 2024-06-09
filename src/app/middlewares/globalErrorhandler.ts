156fe1a669294d58c7a2ddbe9f36ef7d1b00dee0
iamRazzakk <rakibt23p@gmail.com>
2024-06-09 13:16:33 -0400

    work on error handeling

--- src/app/middlewares/globalErrorhandler.ts
+++ src/app/middlewares/globalErrorhandler.ts
@@ -1,23 +1,64 @@
-/* eslint-disable no-undef */
-/* eslint-disable no-unused-vars */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { NextFunction, Request, Response } from 'express';
+import { ErrorRequestHandler } from 'express';
+import { ZodError } from 'zod';
+import { TErrorSources } from '../interface/error';
+import config from '../config';
 
-const globalErrorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction,
+const globalErrorHandler: ErrorRequestHandler = (
+  err,
+  req,
+  res,
+  next,
 ) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'Something went wrong!';
+  let statusCode = err.statusCode || 500;
+  let message = err.message || 'Something went wrong!';
+
+
+
+  let errorSources: TErrorSources = [{
+    path: '',
+    message: "Some thing went wrong"
+  }]
+
+  const handleError = (err: ZodError) => {
+    const errorSources: TErrorSources = err.issues.map((issue) => {
+      return {
+        path: issue.path[issue.path.length - 1],
+        message: issue.message
+      }
+      // const path = issue.path.length - 1;
+      // return path
+    })
+
+    const statusCode = 400
+    return {
+      statusCode,
+      message: "validation Error",
+      errorSources
+    }
+  }
+
+  if (err instanceof ZodError) {
+    const simplifiedError = handleError(err)
+    statusCode = simplifiedError.statusCode
+    message = simplifiedError.message
+    errorSources = simplifiedError.errorSources
+    message: "It's come form zod brother"
+  }
 
   return res.status(statusCode).json({
     success: false,
     message,
-    error: err,
+    // error: err,
+    errorSources,
+    stack: config.NODE_ENV === 'development' ? err?.stack : null
   });
 };
 
 export default globalErrorHandler;
+/**
+ * success
+ * message
+ * errorSources:[
+ * path:"",
+ * message:""
+]*/ 
